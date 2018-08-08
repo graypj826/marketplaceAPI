d90bd42128da53d8be07198d39405c6265be9573
Amanda Molde <amanda.molde@gmail.com>
2018-08-08 11:26:11 -0600

    Added photoURLs to itemSchema

--- models/item.js
+++ models/item.js
@@ -4,7 +4,9 @@ const itemSchema = new mongoose.Schema({
     title: {type: String, required: true},
     description: {type: String, required: true},
     price: {type: Number, required: true},
-    photos: [],
+    photo1URL: {type: String},
+    photo2URL: {type: String},
+    photo3URL: {type: String},
 });
 
 module.exports = mongoose.model('item', itemSchema);
